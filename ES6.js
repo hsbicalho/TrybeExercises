8108bf3b23a4e180845cb94a7c5099a051c0df56
Heitor <hsbicalho@gmail.com>
2022-02-10 21:32:47 -0300

    exercicio 2

--- ES6.js
+++ ES6.js
@@ -1,3 +1,4 @@
+// exercicio 1
 const testingScope = (escopo) => {
   if (escopo === true) {
     var ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
@@ -8,6 +9,15 @@ const testingScope = (escopo) => {
     console.log(elseScope);
   }
   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-}
-
+};
 testingScope(true);
+
+
+
+// exercicio 2
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+oddsAndEvens.sort((a, b) => {
+  return a - b;
+});
+console.log(oddsAndEvens);
+
